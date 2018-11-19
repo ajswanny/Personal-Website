202abd2808228eb04809054e62fee41eb9410ca7
Alexander Swanson <alexander.swanson.1@uvm.edu>
2018-11-19 18:04:41 -0400

    Testing.

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,24 +1,25 @@
 /**
+ * Changes the content for div::#content.
  *
- * @param content_title
+ * @param content_title The content to display.
  */
 function switch_content(content_title) {
 
-    //
+    // Define reference to the #content div and the iframe to contain the new content.
     var
         element = document.querySelector("#content"),
         iframe  = document.createElement("iframe")
     ;
 
-    //
+    // Define the src for the iframe.
     iframe.src = "html/" + content_title + ".html";
 
-    //
+    // Remove all current elements of #content.
     while (element.firstChild) {
         element.removeChild(element.firstChild);
     }
 
-    //
+    // Add the iframe to #content.
     element.appendChild(iframe);
 
 }
